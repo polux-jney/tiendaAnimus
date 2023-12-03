328bac524fc29b1fd10db50d47ad84e265e7239b
polux-jney <gabord4532@gmail.com>
2023-12-03 15:52:53 -0600

    espacio

--- view/footer.php
+++ view/footer.php
@@ -57,6 +57,4 @@
   <!-- Incluir blog -->
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script>
-  
-
 </html>
